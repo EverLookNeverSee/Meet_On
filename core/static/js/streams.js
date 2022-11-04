fd915e8c8e8f53b68b2f2de15d48827d21f8a8e4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-04 18:04:05 +0330

    Implementing handle User joined function

--- core/static/js/streams.js
+++ core/static/js/streams.js
@@ -9,6 +9,7 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on("user-published", handleUserJoined)
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
     let player = `<div class="video-container" id="user-container-${UID}">
@@ -18,6 +19,26 @@ let joinAndDisplayLocalStream = async () => {
     document.getElementById("video-streams").insertAdjacentHTML("beforeend", player)
     localTracks[1].play(`user-${UID}`)
     await client.publish(localTracks[0], localTracks[1])
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.id] = user
+    await client.subscribe(user, mediaType)
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                            <div class="username-wrapper"><span class="user-name">My name</span></div>
+                            <div class="video-player" id="user-${user.uid}"></div>
+                        </div>`
+        document.getElementById("video-streams").insertAdjacentHTML("beforeend", player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
 }
 
 joinAndDisplayLocalStream()
